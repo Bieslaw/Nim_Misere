728894fcd418033cb7e8914351b11b33021b32e8
Meeso1 <39009805+Meeso1@users.noreply.github.com>
2025-04-25 14:16:45 +0200

    Don't freeze ui

--- Ui/RunGameScreen/RunGameScreen.py
+++ Ui/RunGameScreen/RunGameScreen.py
@@ -1,4 +1,4 @@
-from textual import on
+from textual import on, work
 from textual.app import ComposeResult
 from textual.screen import Screen
 from textual.widgets import Button, Header, Footer, Label, Input
@@ -73,13 +73,12 @@ class RunGameScreen(Screen):
         self.previous_stacks = game.stacks.copy()
         self.stack_labels = [Label(f"Stack {i+1}: {size}", classes="stack_label") 
                              for i, size in enumerate(self.game.stacks)]
-        self.limit_input = Input(placeholder="Time Limit", id="limit_input", validators=Number(minimum=0.01))
+        self.limit_input = Input(placeholder="Depth", id="limit_input", validators=Number(minimum=0.01))
         self.change_limit_type_button = Button("Depth", id="limit_type_button", variant="primary")
     
     def compose(self) -> ComposeResult:
         yield Header()
         yield ScrollableContainer(*self.stack_labels, id="stacks_container")
-        # TODO: Choose depth/time limit
         yield Container(
             self.change_limit_type_button,
             self.limit_input,
@@ -100,21 +99,8 @@ class RunGameScreen(Screen):
                 self.app.notify("Time limit must be a positive real number", severity="error")
             return
         
-        if self.limit_input.placeholder == "Depth":
-            self.game.step(int(float(self.limit_input.value)))
-        else:
-            self.game.step_timed(float(self.limit_input.value))
-                
-        # TODO: Modal for game over
-        
-        for i, label in enumerate(self.stack_labels):
-            label.update(f"Stack {i+1}: {self.game.stacks[i]}")
-            if self.game.stacks[i] != self.previous_stacks[i]:
-                label.add_class("last_updated")
-            else:
-                label.remove_class("last_updated")
-            
-        self.previous_stacks = self.game.stacks.copy()
+        self.query_one("#next_move_button").disabled = True
+        self._run_game_step_and_update_ui()
         
     @on(Button.Pressed, "#exit_button")
     def exit(self, event: Button.Pressed) -> None:
@@ -128,5 +114,27 @@ class RunGameScreen(Screen):
     def toggle_limit_type(self, event: Button.Pressed) -> None:
         self.limit_input.placeholder = "Depth" if self.limit_input.placeholder == "Time Limit" else "Time Limit"
         self.change_limit_type_button.label = "Depth" if self.limit_input.placeholder == "Depth" else "Time Limit"
-        self.limit_input.value = ""            
+        self.limit_input.value = "" 
+
+    @work(thread=True, exclusive=True)
+    def _run_game_step_and_update_ui(self) -> None:
+        if self.limit_input.placeholder == "Depth":
+            self.game.step(int(float(self.limit_input.value)))
+        else:
+            self.game.step_timed(float(self.limit_input.value))
+        
+        for i, label in enumerate(self.stack_labels):
+            label.update(f"Stack {i+1}: {self.game.stacks[i]}")
+            if self.game.stacks[i] != self.previous_stacks[i]:
+                label.add_class("last_updated")
+            else:
+                label.remove_class("last_updated")
+                
+        self.previous_stacks = self.game.stacks.copy()
             
+        # TODO: Modal for game over
+        
+        def enable_next_move_button():
+            self.query_one("#next_move_button").disabled = self.game.get_result() is not None
+        
+        self.app.call_from_thread(enable_next_move_button)
