fd6a12aba71a19cc6ba2ba83a69e34a429be46a2
Meeso1 <39009805+Meeso1@users.noreply.github.com>
2025-04-25 13:38:57 +0200

    More stuff

--- Ui/RunGameScreen/RunGameScreen.py
+++ Ui/RunGameScreen/RunGameScreen.py
@@ -1,8 +1,9 @@
 from textual import on
 from textual.app import ComposeResult
 from textual.screen import Screen
-from textual.widgets import Button, Header, Footer, Label
+from textual.widgets import Button, Header, Footer, Label, Input
 from textual.containers import ScrollableContainer, Container
+from textual.validation import Number
 
 from NimMisere import NimMisere
 
@@ -31,6 +32,22 @@ class RunGameScreen(Screen):
         background: green;
     }
     
+    #params_container {
+        layout: horizontal;
+        width: 100%;
+        height: 3;
+        dock: bottom;
+        offset-y: -5;
+    }
+    
+    #limit_input {
+        width: 3fr;
+    }
+    
+    #limit_type_button {
+        width: 1fr;
+    }
+    
     #button_container {
         layout: horizontal;
         width: 100%;
@@ -39,7 +56,7 @@ class RunGameScreen(Screen):
         offset-y: -1;
     }
     
-    #end_game_button {
+    #exit_button {
         height: 3;
         width: 1fr;
     }
@@ -56,21 +73,38 @@ class RunGameScreen(Screen):
         self.previous_stacks = game.stacks.copy()
         self.stack_labels = [Label(f"Stack {i+1}: {size}", classes="stack_label") 
                              for i, size in enumerate(self.game.stacks)]
+        self.limit_input = Input(placeholder="Time Limit", id="limit_input", validators=Number(minimum=0.01))
+        self.change_limit_type_button = Button("Depth", id="limit_type_button", variant="primary")
     
     def compose(self) -> ComposeResult:
         yield Header()
         yield ScrollableContainer(*self.stack_labels, id="stacks_container")
         # TODO: Choose depth/time limit
         yield Container(
-            Button("End Game", id="end_game_button", variant="error"),
+            self.change_limit_type_button,
+            self.limit_input,
+            id="params_container"
+        )
+        yield Container(
+            Button("Exit", id="exit_button", variant="error"),
             Button("Next Move", id="next_move_button", variant="primary"),
             id="button_container"
         )
         yield Footer()
         
     def _advance_game(self) -> None:
-        self.game.step(100)
+        if not self.limit_input.is_valid:
+            if self.limit_input.placeholder == "Depth":
+                self.app.notify("Depth must be a positive integer", severity="error")
+            else:
+                self.app.notify("Time limit must be a positive real number", severity="error")
+            return
         
+        if self.limit_input.placeholder == "Depth":
+            self.game.step(int(float(self.limit_input.value)))
+        else:
+            self.game.step_timed(float(self.limit_input.value))
+                
         # TODO: Modal for game over
         
         for i, label in enumerate(self.stack_labels):
@@ -82,11 +116,17 @@ class RunGameScreen(Screen):
             
         self.previous_stacks = self.game.stacks.copy()
         
-    @on(Button.Pressed, "#end_game_button")
-    def end_game(self, event: Button.Pressed) -> None:
+    @on(Button.Pressed, "#exit_button")
+    def exit(self, event: Button.Pressed) -> None:
         self.dismiss()
         
     @on(Button.Pressed, "#next_move_button")
     def next_move(self, event: Button.Pressed) -> None:
         self._advance_game()
         
+    @on(Button.Pressed, "#limit_type_button")
+    def toggle_limit_type(self, event: Button.Pressed) -> None:
+        self.limit_input.placeholder = "Depth" if self.limit_input.placeholder == "Time Limit" else "Time Limit"
+        self.change_limit_type_button.label = "Depth" if self.limit_input.placeholder == "Depth" else "Time Limit"
+        self.limit_input.value = ""            
+            
